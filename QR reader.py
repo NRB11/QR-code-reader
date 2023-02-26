9057657a624c464c4ae7922aca8727c84adb95ca
NRB11 <34749332+NRB11@users.noreply.github.com>
2023-02-26 03:42:36 +0100

    Update QR reader.py

--- QR reader.py
+++ QR reader.py
@@ -1,17 +1,32 @@
 import cv2
 from pyzbar.pyzbar import decode
+import time
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
-camera = True
+used_code = []
 
+
+
+camera = True
 while camera == True:
     success, frame = cap.read()
     
     for code in decode(frame):
-        print(code.type)
-        print(code.data.decode('utf-8'))
+        if code.data.decode('utf-8') not in used_code:
+            print('Approved')
+            print(code.data.decode('utf-8'))
+            used_code.append(code.data.decode('utf-8'))
+            time.sleep(5) 
+        
+        elif code.data.decode('utf-8') in used_code:
+            print('Not Approved')
+            time.sleep(5)
+        else:
+            pass
     
     cv2.imshow('Testing_QR_scanner', frame)
     cv2.waitKey(1)
+    
+    

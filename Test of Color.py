c984cd45502b0be4c80c6f45a49dd002629da9d7
NRB11 <34749332+NRB11@users.noreply.github.com>
2023-02-27 21:07:40 +0100

    Update Test of Color.py

--- Test of Color.py
+++ Test of Color.py
@@ -21,22 +21,6 @@ while(key != 27):
 
     #Defining the colors
     color = "Undefined"
-    """
-    Colorstate = hue_value
-    match Colorstate:
-        case "< 5":
-            color = "RED"
-        case "< 22":
-            color = "ORANGE"
-        case "< 33":
-            color = "YELLOW"
-        case "< 78":
-            color = "GREEN"
-        case "< 131":
-            color = "BLUE"
-        case "< 170":
-            color = "VIOLET"
-    """         
     if hue_value < 5:
         color = "RED"
     elif hue_value < 22:
@@ -52,52 +36,33 @@ while(key != 27):
     else:
         color = "RED"
     
-        
     #Checking if the camera is pointed at a glove
-if color == "BLUE":
+    if color == "BLUE":
         glove = True
         bluetime += 1
         if bluetime > 100:
             giveItem()
             bluetime = 0
-else:
+    else:
         glove = False
         bluetime = 0
         
-def giveItem():
+    def giveItem():
         #This will work as a function for sending information to the Raspberry PI.
         cv2.putText(frame, "Item Given!",(1170,100),0, 1,(255, 0, 0), 2)
 
 
     #Drawing "UI"
-pixel_center_bgr = frame[cx, cy]
-b, g, r = int(pixel_center_bgr[0]), int(pixel_center_bgr[1]), int(pixel_center_bgr[2])
-cv2.putText(frame, color, (10,50), 0, 1, (b, g, r), 2)
-cv2.putText(frame, str(glove),(1170,50),0, 1,(255, 0, 0), 2)
-cv2.putText(frame, str(bluetime),(1170,200),0, 1,(255, 0, 0), 2)
-cv2.circle(frame, (cy, cx), 5, (25, 25, 25), 3)
+    pixel_center_bgr = frame[cx, cy]
+    b, g, r = int(pixel_center_bgr[0]), int(pixel_center_bgr[1]), int(pixel_center_bgr[2])
+    cv2.putText(frame, color, (10,50), 0, 1, (b, g, r), 2)
+    cv2.putText(frame, str(glove),(1170,50),0, 1,(255, 0, 0), 2)
+    cv2.putText(frame, str(bluetime),(1170,200),0, 1,(255, 0, 0), 2)
+    cv2.circle(frame, (cy, cx), 5, (25, 25, 25), 3)
 
-cv2.imshow("Frame", frame)
-key = cv2.waitKey(1)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1)
 
 #Closing the window(s)
 cap.release()
-cv2.destroyAllWindows() 
-
-
-"""         
-    if hue_value < 5:
-        color = "RED"
-    elif hue_value < 22:
-        color = "ORANGE"
-    elif hue_value < 33:
-        color = "YELLOW"
-    elif hue_value < 78:
-        color = "GREEN"
-    elif hue_value < 131:
-        color = "BLUE"
-    elif hue_value < 170:
-        color = "VIOLET"
-    else:
-        color = "RED"
-    """
+cv2.destroyAllWindows()

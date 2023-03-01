be8f3527cdafbf08f54b9eb76243c8d206da9633
NRB11 <34749332+NRB11@users.noreply.github.com>
2023-03-01 09:35:02 +0100

    update

--- QR reader.py
+++ QR reader.py
@@ -4,7 +4,7 @@ import time
 from subprocess import call
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(3, 640)
 cap.set(4, 480)
 used_code = []
---
+++ test of GUI.py
@@ -0,0 +1,59 @@
+from tkinter import *
+from subprocess import call   
+import cv2
+
+XY = "720x720"
+XL = 730
+YW = 720   
+
+
+class Window(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)        
+        self.master = master
+
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+
+        fileMenu = Menu(menu)
+        fileMenu.add_command(label="Item")
+        fileMenu.add_command(label="Exit", command=self.exitProgram)
+        menu.add_cascade(label="File", menu=fileMenu)
+
+        editMenu = Menu(menu)
+        editMenu.add_command(label="Undo")
+        editMenu.add_command(label="Redo")
+        menu.add_cascade(label="Edit", menu=editMenu)
+        
+        editMenu = Menu(menu)
+        editMenu.add_command(label="QR Reader",command=self.openQR)
+        editMenu.add_command(label="QR.png",command=self.QRpng)
+        menu.add_cascade(label="QR Options", menu=editMenu)
+
+        # widget can take all window
+        self.pack(fill=BOTH, expand=1)
+
+        ##how we make buttons and give them functions
+        #QRButton = Button(self, text="QR Reqder", command=self.openQR)
+        PrintHI = Button(self, text="Print Hi",command=self.PrintHI)
+                
+        #How we place buttons       
+        #QRButton.place(x=350,y=720/2)
+        PrintHI.place(x=XL/2,y=YW/2)
+        
+    def openQR(self):
+        call(["python","QR reader.py"])
+    def QRpng(self):
+        call(["python","1.png"])
+    def exitProgram(self):
+        exit()
+    def PrintHI(self):
+        print("Hi")
+        
+       
+root = Tk()
+root.wm_title("Interface")
+app = Window(root)
+root.geometry(XY)
+root.mainloop()
